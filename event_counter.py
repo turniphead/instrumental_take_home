5f794fded82e4119c75dded9b9e0b9b46a7ed335
dean <darkwinter2424@gmail.com>
2020-05-27 15:12:31 -0700

    add checking for bad inputs

--- event_counter.py
+++ event_counter.py
@@ -1,5 +1,5 @@
-from typing import Optional
 import time
+from typing import Optional
 
 
 class EventCounter:
@@ -8,45 +8,67 @@ class EventCounter:
         # type: (Optional[int]) -> None
         """Create an object to count events.
 
-        Note: This class will maintain a list of length `max_timespan`; be reasonable about how large you make this.
+        Note: This class will maintain a list of length `max_timespan`; be reasonable about how long to make this.
 
-        :param max_timespan: The maximum amount of time, in seconds, for which we can query event counts.
+        Args:
+            max_timespan: The maximum amount of time, in seconds, for which we can query event counts.
         """
         self._max_timespan = max_timespan
         # Initialize a list of length `max_timespan`, where each element is a tuple that represents:
         # (an integer timestamp in seconds since Unix Epoch, the number of events that happened during that second)
         self._event_history = [(0, 0)] * max_timespan
 
-    def record_event(self, timestamp=None):
-        # type: (Optional[float]) -> None
-        """Signal that an event happened to add it to our count.
+    def record_event(self, timestamp=None, number_of_events=1):
+        # type: (Optional[float], Optional[int]) -> None
+        """Signal that an event (or multiple events) happened.
 
-        :param timestamp: The time, seconds since Unix Epoch, that our event occurred. If no timestamp is provided, take
-        the current time instead.
-        :return:
+        Args:
+            timestamp: The time, as seconds since Unix Epoch, that the event occurred. If no timestamp is provided,
+                take the current time instead. Does not allow input times from the future.
+            number_of_events: The number of events that you want to report during this call. All events will be labelled
+                using the same timestamp.
         """
+        now = time.time()
         if timestamp is None:
-            timestamp = time.time()
+            timestamp = now
+        if timestamp > now:
+            raise ValueError(
+                'record_event does not allow timestamps from the future. Current time: {}, Input time: {}'.format(
+                    now, timestamp
+                )
+            )
         # truncate the timestamp to an integer. Note: this rounds down.
         timestamp_int = int(timestamp)
         index = timestamp_int % self._max_timespan
         existing_timestamp, existing_count = self._event_history[index]
         if existing_timestamp == timestamp_int:
-            new_count = existing_count + 1
+            new_count = existing_count + number_of_events
+        elif timestamp_int > existing_timestamp:
+            new_count = number_of_events
         else:
-            new_count = 1
+            # If `timestamp_int` is less than the existing timestamp, it means that this event happened more than
+            # `max_timespan` seconds before the existing timestamp. Don't count this event.
+            new_count = existing_count
         self._event_history[index] = (timestamp_int, new_count)
 
     def request_event_count(self, timespan):
         # type: (int) -> int
         """Request a count of all recorded events that happened in the past `timespan` seconds.
 
-        :param timespan:
-        :return:
+        NOTE: because we round all event occurances down to the nearest second
+
+        Args:
+            timespan: The amount of time to look back when counting events. Must be less than or equal to max_timespan
+                given during initialization.
+
+        Returns:
+            The number of events that have happened in the past `timespan` seconds.
         """
-        assert timespan <= self._max_timespan, \
-            'Timespan requested ({} seconds) was greater than the max timespan provided during EventCounter ' \
-            'Initialization ({} seconds)'.format(timespan, self._max_timespan)
+        if timespan > self._max_timespan:
+            raise ValueError(
+                'Timespan requested ({} seconds) was greater than the max timespan provided during EventCounter '
+                'initialization ({} seconds)'.format(timespan, self._max_timespan)
+            )
         now = int(time.time())
         # Add one because we include the current second in the past `timespan` seconds.
         start_time = now - int(timespan) + 1
